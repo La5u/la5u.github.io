1399fdc87f6063b92e3030134a1522d37d6fca2e
la5u <lasushin@proton.me>
2025-02-02 20:04:19 +0500

    remove preview

--- app.js
+++ app.js
@@ -35,8 +35,8 @@ document.addEventListener('DOMContentLoaded', () => {
     projectCards.forEach(card => observer.observe(card));
 
     // Extract the username from the URL
-    // const username = window.location.hostname.split('.')[0]; // Get the part before ".github.io"
-    const username = 'la5u'
+    const username = window.location.hostname.split('.')[0]; // Get the part before ".github.io"
+    // const username = 'la5u'
 
     // Update GitHub link
     const githubLink = document.querySelector('.github-link');
@@ -46,48 +46,5 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.github-link[data-repo]').forEach(link => {
         const repo = link.getAttribute('data-repo');
         link.href = `https://github.com/${username}/${repo}`;
-    });
-});
-
-async function fetchPreviewImage(url) {
-    try {
-        console.log(`Fetching image from URL: ${url}`); // Log the URL being fetched
-        const response = await fetch(url);
-        // Check if the response is OK (status in the range 200-299)
-        if (!response.ok) {
-            console.log(`Error fetching image: ${response.status} ${response.statusText}`);
-            return null; // Return null if the response is not OK
-        }
-        const screenshot = await new Promise((resolve, reject) => {
-            html2canvas(document.body, { scale: 4 }).then(canvas => { // Increased scale for a more pronounced zoom effect
-                const dataUrl = canvas.toDataURL('image/png');
-                resolve(dataUrl); // Return the screenshot URL
-            }).catch(error => {
-                console.error('Error taking screenshot:', error);
-                reject(null);
-            });
-        });
-        return screenshot; // Return the screenshot URL
-    } catch (error) {
-        console.log(`Fetch error: ${error}`);
-        return null;
-    }
-}
-document.querySelectorAll('.project-card').forEach(card => {
-    card.addEventListener('mouseover', async () => {
-        const previewImage = card.querySelector('.preview-image');
-
-        // Use a flag to ensure the image is fetched only once
-        if (!previewImage.src && !previewImage.dataset.fetched) {
-            const link = card.querySelector('.project-links a'); // Get the first link in the project card
-            
-            previewImage.dataset.fetched = true; // Set the flag to indicate the image has been fetched
-            const imageUrl = await fetchPreviewImage(link.href);
-            if (imageUrl) {
-                console.log(imageUrl); // Log the image URL
-                previewImage.src = imageUrl;
-                previewImage.style.display = 'block'; // Correctly set the display property
-            }
-        }
     });
 });
